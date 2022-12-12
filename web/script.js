c431b221a4be65a97b1696e38801835c79a2fae9
yashasbm0204 <120355763+yashasbm0204@users.noreply.github.com>
2022-12-12 15:58:33 +0530

    Add files via upload

---
+++ web/script.js
@@ -0,0 +1,16 @@
+function btnclick()
+{
+    let status= document.getElementById("btn").innerHTML
+    if(status==="on")
+        document.getElementById("btn").innerHTML="off";
+        else
+        document.getElementById("btn").innerHTML="on";
+}
+let count=0;
+function inc()
+{
+    count=count+1;
+    document.getElementById("inc")
+    
+
+}
